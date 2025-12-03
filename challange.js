a7b966de6840d4435a1e10588268389f97af81b9
FuzeHere <fairuzofficial7@gmail.com>
2025-12-03 21:03:49 +0800

    Challange 1 from AH

---
+++ challange.js
@@ -0,0 +1,11 @@
+const nilai = 400;
+
+if (nilai === 5) {
+    console.log('Satuan');
+} else if (nilai === 14) {
+    console.log('Ratusan');
+} else if (nilai >= 1000) {
+    console.log(nilai)
+} else {
+    console.log('Bukan angka yang diminta!')
+}
