6b1631a1aa227459d07e65e07018d8c5ef26da55
FuzeHere <fairuzofficial7@gmail.com>
2025-12-06 22:46:56 +0800

    repair and update

--- challange.js
+++ challange.js
@@ -1,9 +1,11 @@
-const nilai = 400;
+const nilai = 14;
 
 if (nilai === 5) {
     console.log('Satuan');
 } else if (nilai === 14) {
-    console.log('Ratusan');
+    console.log('Puluhan');
+} else if (nilai >= 100) {
+    console.log("Ratusan")
 } else if (nilai >= 1000) {
     console.log(nilai)
 } else {
